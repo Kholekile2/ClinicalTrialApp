c03312651fbfb0a09e589b18c7bd9fdc9de0ef29
Kholekile2 <5100146@mypillar7uwc.ac.za>
2024-05-06 14:19:06 +0200

    first commit

---
+++ ClinicalTrial/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ClinicalTrial
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
